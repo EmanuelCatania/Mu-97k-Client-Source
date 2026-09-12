d3792754a23cfb10099925274f401895baf15a5f
Emanuel B. Catania <emanuelb.catania@gmail.com>
2026-09-12 16:24:13 -0300

    Costo de reparar todo: recorrer el grid del inventario, no &DAT_07ea8410
    sub_4C4080 suma, ademas del equipo, las 64 celdas de OffsetInventoryItems
    (hasta 0x7EA9510) con Key. El port recorria 8 items desde &DAT_07ea8410, un
    DWORD suelto en este build: leia globals vecinos (las coordenadas del
    tooltip, entre otros) y el costo cambiaba segun el item bajo el mouse.

--- src/Item/Item_Durability.cpp
+++ src/Item/Item_Durability.cpp
@@ -487,9 +487,14 @@ void __cdecl Item_RecalculateRepairCost(void)
         }
     }
 
-    // Iterate 8 extra item slots (DAT_07ea8410, stride 0x44, short* step = 0x22)
-    short *psVar12 = (short *)&DAT_07ea8410;
-    for (int i = 0; i < 8; i++, psVar12 += 0x22) {
+    // IDA sub_4C4080: segundo bucle sobre el grid del inventario,
+    // OffsetInventoryItems .. 0x7EA9510 = 64 celdas de 0x44 (8x8), filtrando
+    // por Key.  2026-09-12: el port recorria 8 "items" desde &DAT_07ea8410, que
+    // en este build es un DWORD suelto: leia los globals vecinos (entre ellos
+    // DAT_07ea840c/8408, las coordenadas del tooltip) y el costo de "reparar
+    // todo" cambiaba segun el item bajo el mouse.
+    short *psVar12 = (short *)OffsetInventoryItems;
+    for (int i = 0; i < 64; i++, psVar12 += 0x22) {
         if (*(int *)((char *)psVar12 + 0x38) != 0) {
             short itemType = *psVar12;
             unsigned int maxDur = Item_CalculateMaxDurability(psVar12, (int)itemType * 0x40 + (int)DAT_07d78068,
